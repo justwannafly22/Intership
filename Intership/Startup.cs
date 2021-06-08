ffb7ae980420810f05e02bfb466d6c558a9d38c2
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-08 18:37:08 +0300

    Added processing the errors in app

--- Intership/Startup.cs
+++ Intership/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NLog;
+using System.IO;
 
 namespace Intership
 {
@@ -13,9 +15,10 @@ namespace Intership
     {
         public Startup(IConfiguration configuration)
         {
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
             Configuration = configuration;
         }
-
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -24,6 +27,7 @@ namespace Intership
             services.AddControllersWithViews();
 
             services.ConfigureSqlContext(Configuration);
+            services.ConfigureLoggerService();
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
