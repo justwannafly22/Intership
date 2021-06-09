76205e1ee2ff51c70a40ebc93c8167eaddc523fb
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-09 09:59:17 +0300

    Added one sql script

---
+++ DbUp/Scripts/OneMoreFeed.sql
@@ -0,0 +1,5 @@
+INSERT INTO Clients
+VALUES
+('Vladimir', 'Medvedev', 50, '+375 (29) 098-33-56', 'volodka@mail.ru'),
+('Dima', 'Iphonov', 23, '+375 (29) 628-73-36', 'dimka@mail.ru'),
+('Ilya', 'Dellov', 45, '+375 (29) 123-23-96', 'knizhnaya1a@mail.ru')
