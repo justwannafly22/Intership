e1e8da71127a328e5200df568fca7a6c260f271b
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-10 15:13:48 +0300

    Update ClientRepository.cs

--- Intership.Data/ClientRepository.cs
+++ Intership.Data/ClientRepository.cs
@@ -30,10 +30,5 @@ namespace Intership.Data
         public async Task<IEnumerable<Client>> GetClientsAsync(bool trackChanges) =>
             await FindAll(trackChanges)
             .ToListAsync();
-
-        public async Task UpdateClientAsync(Client client)
-        {
-            await UpdateAsync(client);
-        }
     }
 }
