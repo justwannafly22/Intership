52e0006dba84edd20835b0a3709c36d1630db984
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-10 15:28:06 +0300

    Create ClientController.cs

---
+++ Intership/Controllers/ClientController.cs
@@ -0,0 +1,24 @@
+﻿using Intership.Abstracts.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intership.Controllers
+{
+    [ApiController]
+    [Route("api/clients")]
+    public class ClientController : Controller
+    {
+        private readonly IClientLogic _clientLogic;
+
+        public ClientController(IClientLogic clientLogic)
+        {
+            _clientLogic = clientLogic;
+        }
+
+
+
+    }
+}
