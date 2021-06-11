836d2ab1b5dccd3eccc7d2a7db0cb43d03ef8367
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-11 13:09:38 +0300

    Implemented product repository

---
+++ Intership.Data/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using Intership.Abstracts.Repositories;
+using Intership.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intership.Data
+{
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(MyDbContext context)
+            : base(context)
+        {
+        }
+
+        public async Task CreateProductAsync(Product product) =>
+            await CreateAsync(product);
+
+        public async Task DeleteProductAsync(Product product) =>
+            await DeleteAsync(product);
+
+        public async Task<Product> GetProductAsync(Guid id, bool trackChanges) =>
+            await FindByCondition(p => p.Id.Equals(id), trackChanges)
+            .SingleOrDefaultAsync();
+
+        public async Task<IEnumerable<Product>> GetProductsAsync(bool trackChanges) =>
+            await FindAll(trackChanges)
+            .ToListAsync();
+
+        public async Task UpdateProductAsync(Product product) =>
+            await UpdateProductAsync(product);
+    }
+}
