673089538837ea5085e3f3c3412105bafc1b08c7
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-11 13:17:34 +0300

    Implemented product logic

---
+++ Intership.Abstracts/Logic/IProductLogic.cs
@@ -0,0 +1,17 @@
+﻿using Intership.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intership.Abstracts.Logic
+{
+    public interface IProductLogic
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductAsync(Guid id);
+        Task CreateProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(Product product);
+    }
+}
