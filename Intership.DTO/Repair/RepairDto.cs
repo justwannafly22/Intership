f4187109826c63cdaaf83a7de49d258a46686676
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-11 16:13:17 +0300

    Added repair dto`s

---
+++ Intership.DTO/Repair/RepairDto.cs
@@ -0,0 +1,17 @@
+﻿using Intership.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intership.DTO.Repair
+{
+    public class RepairDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string AdvancedInfo { get; set; }
+        public string Status { get; set; }
+        public ICollection<ReplacedPart> ReplacedParts { get; set; }
+    }
+}
---
+++ Intership.DTO/Repair/RepairForManipulationDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Intership.DTO.Repair
+{
+    public abstract class RepairForManipulationDto
+    {
+        [Required(ErrorMessage = "Name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Max length for Name is 60 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Date is a required field.")]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Advanced info is a required field.")]
+        [MaxLength(500, ErrorMessage = "Max length for AdvancedInfo is 500 characters.")]
+        public string AdvancedInfo { get; set; }
+    }
+}
