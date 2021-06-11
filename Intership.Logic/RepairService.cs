ba92f03f19b6c808048261f36d9cfd1421983bb6
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-11 17:26:14 +0300

    Implemented Repair logic

---
+++ Intership.Logic/RepairService.cs
@@ -0,0 +1,51 @@
+﻿using Intership.Abstracts.Repositories;
+using Intership.Abstracts.Services;
+using Intership.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intership.Logic
+{
+    public class RepairService : IRepairService
+    {
+        private readonly IRepairRepository _repairRepository;
+
+        public RepairService(IRepairRepository repairRepository)
+        {
+            _repairRepository = repairRepository;
+        }
+        
+        public async Task CreateRepairAsync(Guid productId, Repair repair)
+        {
+            repair.ProductId = productId;
+
+            await _repairRepository.CreateRepairAsync(repair);
+        }
+
+        public async Task DeleteRepairAsync(Repair repair)
+        {
+            await _repairRepository.DeleteRepairAsync(repair);
+        }
+
+        public async Task<Repair> GetRepairAsync(Guid repairId, Guid productId)
+        {
+            var repair = await _repairRepository.GetRepairAsync(repairId, productId, trackChanges: true);
+
+            return repair;
+        }
+
+        public async Task<IEnumerable<Repair>> GetRepairsAsync(Guid productId)
+        {
+            var repairs = await _repairRepository.GetRepairsAsync(productId, trackChanges: false);
+
+            return repairs;
+        }
+
+        public async Task UpdateRepairAsync(Repair repair)
+        {
+            await _repairRepository.UpdateRepairAsync(repair);
+        }
+    }
+}
