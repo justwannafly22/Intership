27381f238c453f9d7472151a8eeb45086f6e7d07
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-11 16:21:55 +0300

    Implemented Repair repository

---
+++ Intership.Abstracts/Repositories/IRepairRepository.cs
@@ -0,0 +1,20 @@
+﻿using Intership.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intership.Abstracts.Repositories
+{
+    /// <summary>
+    /// Repair repository behavior
+    /// </summary>
+    public interface IRepairRepository
+    {
+        Task<IEnumerable<Repair>> GetRepairsAsync(bool trackChanges);
+        Task<Repair> GetRepairAsync(Guid id, bool trackChanges);
+        Task CreateRepairAsync(Repair repair);
+        Task UpdateRepairAsync(Repair repair);
+        Task DeleteRepairAsync(Repair repair);
+    }
+}
---
+++ Intership.Data/RepairRepository.cs
@@ -0,0 +1,35 @@
+﻿using Intership.Abstracts.Repositories;
+using Intership.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intership.Data
+{
+    public class RepairRepository : RepositoryBase<Repair>, IRepairRepository
+    {
+        public RepairRepository(MyDbContext context)
+            : base(context)
+        {
+        }
+
+        public async Task CreateRepairAsync(Repair repair) =>
+            await CreateAsync(repair);
+
+        public async Task DeleteRepairAsync(Repair repair) =>
+            await DeleteAsync(repair);
+
+        public async Task<Repair> GetRepairAsync(Guid id, bool trackChanges) =>
+            await FindByCondition(r => r.Id.Equals(id), trackChanges)
+            .SingleOrDefaultAsync();
+
+        public async Task<IEnumerable<Repair>> GetRepairsAsync(bool trackChanges) =>
+            await FindAll(trackChanges)
+            .ToListAsync();
+
+        public async Task UpdateRepairAsync(Repair repair) =>
+            await UpdateAsync(repair);
+    }
+}
