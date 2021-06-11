f90dcc418e1c89688d67c834fd4721e604c43842
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-11 12:35:28 +0300

    Added product dto`s

---
+++ Intership.DTO/Product/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intership.DTO.Product
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Intership.DTO/Product/ProductForManipulationDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Intership.DTO.Product
+{
+    public abstract class ProductForManipulationDto
+    {
+        [Required(ErrorMessage = "Name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Max length for Name is 60 characters.")]
+        public string Name { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Price is required and it can`t be lower than 0.")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "Product description is a required field.")]
+        public string Description { get; set; }
+    }
+}
