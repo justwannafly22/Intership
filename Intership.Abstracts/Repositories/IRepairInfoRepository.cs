2213a4bca740a35ced7ac4f30d3af37eb1c98a7f
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-11 16:31:03 +0300

    Implemented RepairInfo repository

---
+++ Intership.Abstracts/Repositories/IRepairInfoRepository.cs
@@ -0,0 +1,16 @@
+﻿using Intership.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intership.Abstracts.Repositories
+{
+    public interface IRepairInfoRepository
+    {
+        Task<RepairInfo> GetRepairInfoAsync(Guid id, bool trackChanges);
+        Task CreateRepairInfo(RepairInfo repairInfo);
+        Task UpdateRepairInfo(RepairInfo repairInfo);
+        Task DeleteRepairInfo(RepairInfo repairInfo);
+    }
+}
---
+++ Intership.Data/RepairInfoRepository.cs
@@ -0,0 +1,29 @@
+﻿using Intership.Abstracts.Repositories;
+using Intership.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace Intership.Data
+{
+    public class RepairInfoRepository : RepositoryBase<RepairInfo>, IRepairInfoRepository
+    {
+        public RepairInfoRepository(MyDbContext context)
+            : base(context)
+        {
+        }
+
+        public async Task CreateRepairInfo(RepairInfo repairInfo) =>
+            await CreateAsync(repairInfo);
+
+        public async Task DeleteRepairInfo(RepairInfo repairInfo) =>
+            await DeleteAsync(repairInfo);
+
+        public async Task<RepairInfo> GetRepairInfoAsync(Guid id, bool trackChanges) =>
+            await FindByCondition(r => r.Id.Equals(id), trackChanges)
+            .SingleOrDefaultAsync();
+
+        public async Task UpdateRepairInfo(RepairInfo repairInfo) =>
+            await UpdateAsync(repairInfo);
+    }
+}
