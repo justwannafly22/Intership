f839428bf1dbbc0bbee08d412ea4707d29fff4e9
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-11 16:25:37 +0300

    Added RepairInfo id

--- Intership.DTO/Repair/RepairDto.cs
+++ Intership.DTO/Repair/RepairDto.cs
@@ -12,6 +12,7 @@ namespace Intership.DTO.Repair
         public DateTime Date { get; set; }
         public string AdvancedInfo { get; set; }
         public string Status { get; set; }
+        public Guid RepairInfoId { get; set; }
         public ICollection<ReplacedPart> ReplacedParts { get; set; }
     }
 }
