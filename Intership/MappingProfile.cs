88faedd19ffa82af007d5b3f6301a804615950f7
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-14 11:10:55 +0300

    Added RepairDto

--- Intership/MappingProfile.cs
+++ Intership/MappingProfile.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Intership.DTO.Client;
 using Intership.DTO.Product;
+using Intership.DTO.Repair;
 using Intership.Models.Entities;
 
 namespace Intership
@@ -29,6 +30,22 @@ namespace Intership
             CreateMap<ProductForCreateDto, Product>();
 
             CreateMap<ProductForUpdateDto, Product>().ReverseMap();
+            #endregion
+
+            #region Repair
+            CreateMap<Repair, RepairDto>()
+                .IncludeMembers(source => source.RepairInfo, source => source.RepairInfo.Status);
+
+            CreateMap<RepairInfo, RepairDto>()
+                .ForMember(r => r.Date,
+                    opt => opt.MapFrom(x => x.Date))
+                .ForMember(r => r.AdvancedInfo,
+                    opt => opt.MapFrom(x => x.AdvancedInfo));
+
+            CreateMap<Status, RepairDto>()
+                .ForMember(r => r.StatusInfo,
+                    opt => opt.MapFrom(x => x.StatusInfo));
+
             #endregion
         }
     }
