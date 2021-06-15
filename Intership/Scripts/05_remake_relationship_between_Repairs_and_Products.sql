6e001b4a92b560390f2ded1cce2fbeb7188cd174
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-15 16:17:40 +0300

    Update 05_remake_relationship_between_Repairs_and_Products.sql

--- Intership/Scripts/05_remake_relationship_between_Repairs_and_Products.sql
+++ Intership/Scripts/05_remake_relationship_between_Repairs_and_Products.sql
@@ -1,9 +1,17 @@
 --ALTER TABLE Repairs
 --DROP CONSTRAINT FK_product_id
 
-CREATE TABLE RepairsProducts(
-repair_id UNIQUEIDENTIFIER NOT NULL,
-product_id UNIQUEIDENTIFIER NOT NULL,
-FOREIGN KEY (repair_id) REFERENCES Repairs (id),
-FOREIGN KEY (product_id) REFERENCES Products (id)
-);
+--CREATE TABLE RepairsProducts(
+--repair_id UNIQUEIDENTIFIER NOT NULL,
+--product_id UNIQUEIDENTIFIER NOT NULL,
+--FOREIGN KEY (repair_id) REFERENCES Repairs (id),
+--FOREIGN KEY (product_id) REFERENCES Products (id)
+--);
+
+--ALTER TABLE Repairs
+--DROP CONSTRAINT FK__Repairs__product__20E1DCB5
+
+--DROP INDEX IX_Repairs_product_id ON Repairs
+
+ALTER TABLE Repairs
+DROP COLUMN product_id
