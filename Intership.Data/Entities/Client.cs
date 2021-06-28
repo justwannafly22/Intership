966296806c1334a4bbb20b2572a9f3826bd137ec
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-06-28 17:57:04 +0300

    Replaced entities files

---
+++ Intership.Data/Entities/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Intership.Data.Entities
+{
+    public class Client
+    {
+        [Column("id")]
+        [Key]
+        public Guid Id { get; set; }
+
+        [Column("client_name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Column("client_surname")]
+        [Required]
+        public string Surname { get; set; }
+
+        [Column("age")]
+        [Required]
+        public int Age { get; set; }
+
+        [Column("contact_number")]
+        [Required]
+        public string ContactNumber { get; set; }
+
+        [Column("email")]
+        [Required]
+        public string Email { get; set; }
+        
+        [Column("allow_email_notifications")]
+        [Required]
+        public bool AllowEmailNotification { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ Intership.Data/Entities/Repair.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Intership.Data.Entities
+{
+    public class Repair
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("name")]
+        [Required(ErrorMessage = "Name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Max length for Name is 60 characters.")]
+        public string Name { get; set; }
+
+        [ForeignKey(nameof(RepairInfo))]
+        [Column("repairinfo_id")]
+        public Guid RepairInfoId { get; set; }
+        public RepairInfo RepairInfo { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+        public ICollection<ReplacedPart> ReplacedParts { get; set; }
+    }
+}
