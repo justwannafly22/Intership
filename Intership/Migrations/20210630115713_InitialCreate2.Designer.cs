39909c5564fe37b0a7145d19e23ab7da81d86a96
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-07-02 19:28:29 +0300

    Update migrations

--- Intership/Migrations/20210630115713_InitialCreate2.Designer.cs
+++ Intership/Migrations/20210630115713_InitialCreate2.Designer.cs
@@ -4,14 +4,16 @@ using Intership.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Intership.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210630115713_InitialCreate2")]
+    partial class InitialCreate2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -161,6 +163,135 @@ namespace Intership.Migrations
                         });
                 });
 
+            modelBuilder.Entity("Intership.Data.Entities.Order", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uniqueidentifier")
+                        .HasColumnName("id");
+
+                    b.Property<string>("AdvancedInfo")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)")
+                        .HasColumnName("advanced_info");
+
+                    b.Property<Guid>("ClientId")
+                        .HasColumnType("uniqueidentifier")
+                        .HasColumnName("client_id");
+
+                    b.Property<int>("Count")
+                        .HasColumnType("int")
+                        .HasColumnName("count");
+
+                    b.Property<DateTime>("Date")
+                        .HasColumnType("datetime2")
+                        .HasColumnName("order_date");
+
+                    b.Property<Guid>("ProductId")
+                        .HasColumnType("uniqueidentifier")
+                        .HasColumnName("product_id");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("ClientId");
+
+                    b.HasIndex("ProductId");
+
+                    b.ToTable("Orders");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = new Guid("b2333228-6d8e-43ae-8009-95e64ea50938"),
+                            AdvancedInfo = "Advanced info is empty",
+                            ClientId = new Guid("116f6c7f-cc8c-4526-a3d5-ca799bf440a2"),
+                            Count = 1,
+                            Date = new DateTime(2021, 6, 8, 14, 35, 0, 0, DateTimeKind.Unspecified),
+                            ProductId = new Guid("80abbca8-554d-4b16-b5de-024705497d4a")
+                        },
+                        new
+                        {
+                            Id = new Guid("0843b6af-8160-42e3-a65e-40a897851c8c"),
+                            AdvancedInfo = "Advanced info is empty",
+                            ClientId = new Guid("116f6c7f-cc8c-4526-a3d5-ca799bf440a2"),
+                            Count = 1,
+                            Date = new DateTime(2021, 6, 8, 14, 35, 15, 0, DateTimeKind.Unspecified),
+                            ProductId = new Guid("3fc64654-f1e8-4563-ad8f-98c4c56a548a")
+                        },
+                        new
+                        {
+                            Id = new Guid("5d96d2dc-c8be-4846-9066-da88f1d2934a"),
+                            AdvancedInfo = "Advanced info is empty",
+                            ClientId = new Guid("116f6c7f-cc8c-4526-a3d5-ca799bf440a2"),
+                            Count = 1,
+                            Date = new DateTime(2021, 6, 8, 14, 35, 30, 0, DateTimeKind.Unspecified),
+                            ProductId = new Guid("95bdd46a-f616-4188-9f1a-3dc2eb1a3a64")
+                        },
+                        new
+                        {
+                            Id = new Guid("ac16b9e3-6bc2-43ee-b78f-7c7c2f3b084e"),
+                            AdvancedInfo = "Advanced info is empty",
+                            ClientId = new Guid("9ff78f1f-701d-4d30-a619-e0d9e5a4cb65"),
+                            Count = 1,
+                            Date = new DateTime(2021, 6, 8, 11, 30, 0, 0, DateTimeKind.Unspecified),
+                            ProductId = new Guid("23cbb20e-8174-4ab6-b32e-ee2010f19217")
+                        },
+                        new
+                        {
+                            Id = new Guid("5ea3751a-a36a-480b-b6b1-f78947b70b19"),
+                            AdvancedInfo = "Advanced info is empty",
+                            ClientId = new Guid("9ff78f1f-701d-4d30-a619-e0d9e5a4cb65"),
+                            Count = 1,
+                            Date = new DateTime(2021, 6, 8, 10, 10, 50, 0, DateTimeKind.Unspecified),
+                            ProductId = new Guid("9def4539-eb0b-4f28-ac0c-d7f4d7e18de0")
+                        },
+                        new
+                        {
+                            Id = new Guid("48784c17-efa4-41ab-9ff9-bdc676d7d20a"),
+                            AdvancedInfo = "Advanced info is empty",
+                            ClientId = new Guid("6beee7e9-8f29-4a6b-854b-98c87759cb2d"),
+                            Count = 1,
+                            Date = new DateTime(2021, 6, 8, 15, 15, 37, 0, DateTimeKind.Unspecified),
+                            ProductId = new Guid("338afc0c-7290-4c15-a5f5-cb091dea6ad3")
+                        },
+                        new
+                        {
+                            Id = new Guid("cb63b643-b3c3-4043-a710-6d6d02f02be8"),
+                            AdvancedInfo = "Advanced info is empty",
+                            ClientId = new Guid("c766948e-6e43-4c37-a340-71a06347376d"),
+                            Count = 1,
+                            Date = new DateTime(2021, 6, 8, 9, 57, 13, 0, DateTimeKind.Unspecified),
+                            ProductId = new Guid("3af8f791-6c93-4afb-820d-3c8bccc83bc4")
+                        },
+                        new
+                        {
+                            Id = new Guid("6fdff5b9-69c9-4f4b-88fe-936968128ea2"),
+                            AdvancedInfo = "Advanced info is empty",
+                            ClientId = new Guid("4987cad9-aabd-4cab-8ef2-cc2c6283da29"),
+                            Count = 1,
+                            Date = new DateTime(2021, 6, 7, 13, 27, 23, 0, DateTimeKind.Unspecified),
+                            ProductId = new Guid("d3bfdeae-ad9e-44e2-b666-52e758c19dda")
+                        },
+                        new
+                        {
+                            Id = new Guid("9c26b939-fb16-493e-954e-5efe09ebf11f"),
+                            AdvancedInfo = "Advanced info is empty",
+                            ClientId = new Guid("1d40d73b-9f63-43f6-8eba-ef91d54f1f08"),
+                            Count = 1,
+                            Date = new DateTime(2021, 6, 7, 5, 2, 45, 0, DateTimeKind.Unspecified),
+                            ProductId = new Guid("3af8f791-6c93-4afb-820d-3c8bccc83bc4")
+                        },
+                        new
+                        {
+                            Id = new Guid("15b6e738-a210-40aa-97b0-58798f4339af"),
+                            AdvancedInfo = "Advanced info is empty",
+                            ClientId = new Guid("2acefc95-a35c-4f0a-848a-665ab939e6b8"),
+                            Count = 2,
+                            Date = new DateTime(2021, 6, 7, 23, 12, 4, 0, DateTimeKind.Unspecified),
+                            ProductId = new Guid("cae134c8-1faf-4e65-b08d-864367afeb21")
+                        });
+                });
+
             modelBuilder.Entity("Intership.Data.Entities.Product", b =>
                 {
                     b.Property<Guid>("Id")
@@ -267,10 +398,6 @@ namespace Intership.Migrations
                         .HasColumnType("uniqueidentifier")
                         .HasColumnName("id");
 
-                    b.Property<Guid>("ClientId")
-                        .HasColumnType("uniqueidentifier")
-                        .HasColumnName("client_id");
-
                     b.Property<string>("Name")
                         .IsRequired()
                         .HasMaxLength(60)
@@ -283,78 +410,66 @@ namespace Intership.Migrations
 
                     b.HasKey("Id");
 
-                    b.HasIndex("ClientId");
-
                     b.ToTable("Repairs");
 
                     b.HasData(
                         new
                         {
                             Id = new Guid("b70d7e3e-35c7-4c24-acda-1472ae0c1d55"),
-                            ClientId = new Guid("4ed8b5d0-01cb-4c1c-921f-0a58dab32f69"),
                             Name = "Fixing the problem",
                             RepairInfoId = new Guid("00000000-0000-0000-0000-000000000000")
                         },
                         new
                         {
                             Id = new Guid("228aea49-4717-4bf9-9cf4-585b33e54629"),
-                            ClientId = new Guid("c80a9219-4ff3-435a-be8a-3462482a73d9"),
                             Name = "Fixing the problem",
                             RepairInfoId = new Guid("00000000-0000-0000-0000-000000000000")
                         },
                         new
                         {
                             Id = new Guid("260e7b03-cf9e-4824-8e3d-174f6fb73e49"),
-                            ClientId = new Guid("51032953-9150-4072-9a56-ea22906e799c"),
                             Name = "Fixing the problem",
                             RepairInfoId = new Guid("00000000-0000-0000-0000-000000000000")
                         },
                         new
                         {
                             Id = new Guid("7478668c-1c7a-47ad-82c8-48c3744a1fdb"),
-                            ClientId = new Guid("2acefc95-a35c-4f0a-848a-665ab939e6b8"),
                             Name = "Fixing the problem",
                             RepairInfoId = new Guid("00000000-0000-0000-0000-000000000000")
                         },
                         new
                         {
                             Id = new Guid("7d844508-2b92-4fe5-afc8-5e05382b9761"),
-                            ClientId = new Guid("1d40d73b-9f63-43f6-8eba-ef91d54f1f08"),
                             Name = "Fixing the problem",
                             RepairInfoId = new Guid("00000000-0000-0000-0000-000000000000")
                         },
                         new
                         {
                             Id = new Guid("633693ac-2891-4fa9-99e9-ffbb9d33aa4b"),
-                            ClientId = new Guid("116f6c7f-cc8c-4526-a3d5-ca799bf440a2"),
                             Name = "Fixing the problem",
                             RepairInfoId = new Guid("00000000-0000-0000-0000-000000000000")
                         },
                         new
                         {
                             Id = new Guid("530f0ff1-2d7e-4be0-80e3-77453658c9de"),
-                            ClientId = new Guid("9ff78f1f-701d-4d30-a619-e0d9e5a4cb65"),
                             Name = "Fixing the problem",
                             RepairInfoId = new Guid("00000000-0000-0000-0000-000000000000")
                         },
                         new
                         {
                             Id = new Guid("04af2669-ed99-4517-9559-afd077ecd737"),
-                            ClientId = new Guid("6beee7e9-8f29-4a6b-854b-98c87759cb2d"),
                             Name = "Fixing the problem",
                             RepairInfoId = new Guid("00000000-0000-0000-0000-000000000000")
                         },
                         new
                         {
                             Id = new Guid("c016f3c3-5189-469a-a580-1798d88882ee"),
-                            ClientId = new Guid("c766948e-6e43-4c37-a340-71a06347376d"),
                             Name = "Fixing the problem",
                             RepairInfoId = new Guid("00000000-0000-0000-0000-000000000000")
                         },
                         new
                         {
                             Id = new Guid("29b9d37c-3f23-4507-96b5-8506b291218b"),
-                            ClientId = new Guid("4987cad9-aabd-4cab-8ef2-cc2c6283da29"),
                             Name = "Fixing the problem",
                             RepairInfoId = new Guid("00000000-0000-0000-0000-000000000000")
                         });
@@ -668,15 +783,38 @@ namespace Intership.Migrations
                         });
                 });
 
-            modelBuilder.Entity("Intership.Data.Entities.Repair", b =>
+            modelBuilder.Entity("ProductRepair", b =>
+                {
+                    b.Property<Guid>("ProductsId")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<Guid>("RepairsId")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.HasKey("ProductsId", "RepairsId");
+
+                    b.HasIndex("RepairsId");
+
+                    b.ToTable("ProductRepair");
+                });
+
+            modelBuilder.Entity("Intership.Data.Entities.Order", b =>
                 {
                     b.HasOne("Intership.Data.Entities.Client", "Client")
-                        .WithMany("Repairs")
+                        .WithMany("Orders")
                         .HasForeignKey("ClientId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
+                    b.HasOne("Intership.Data.Entities.Product", "Product")
+                        .WithMany("Orders")
+                        .HasForeignKey("ProductId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
                     b.Navigation("Client");
+
+                    b.Navigation("Product");
                 });
 
             modelBuilder.Entity("Intership.Data.Entities.RepairInfo", b =>
@@ -717,13 +855,30 @@ namespace Intership.Migrations
                     b.Navigation("Repair");
                 });
 
+            modelBuilder.Entity("ProductRepair", b =>
+                {
+                    b.HasOne("Intership.Data.Entities.Product", null)
+                        .WithMany()
+                        .HasForeignKey("ProductsId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("Intership.Data.Entities.Repair", null)
+                        .WithMany()
+                        .HasForeignKey("RepairsId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+                });
+
             modelBuilder.Entity("Intership.Data.Entities.Client", b =>
                 {
-                    b.Navigation("Repairs");
+                    b.Navigation("Orders");
                 });
 
             modelBuilder.Entity("Intership.Data.Entities.Product", b =>
                 {
+                    b.Navigation("Orders");
+
                     b.Navigation("ReplacedParts");
                 });
 
---
+++ Intership/Migrations/20210630115713_InitialCreate2.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Intership.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Repairs_Products_product_id",
+                table: "Repairs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Repairs_product_id",
+                table: "Repairs");
+
+            migrationBuilder.DropColumn(
+                name: "product_id",
+                table: "Repairs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "contact_number",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "client_surname",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "client_name",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.CreateTable(
+                name: "ProductRepair",
+                columns: table => new
+                {
+                    ProductsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RepairsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductRepair", x => new { x.ProductsId, x.RepairsId });
+                    table.ForeignKey(
+                        name: "FK_ProductRepair_Products_ProductsId",
+                        column: x => x.ProductsId,
+                        principalTable: "Products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductRepair_Repairs_RepairsId",
+                        column: x => x.RepairsId,
+                        principalTable: "Repairs",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductRepair_RepairsId",
+                table: "ProductRepair",
+                column: "RepairsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductRepair");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "product_id",
+                table: "Repairs",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "Clients",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "contact_number",
+                table: "Clients",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "client_surname",
+                table: "Clients",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "client_name",
+                table: "Clients",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Repairs",
+                keyColumn: "id",
+                keyValue: new Guid("04af2669-ed99-4517-9559-afd077ecd737"),
+                column: "product_id",
+                value: new Guid("e357485f-cddd-4c92-8de7-702918e2bda8"));
+
+            migrationBuilder.UpdateData(
+                table: "Repairs",
+                keyColumn: "id",
+                keyValue: new Guid("228aea49-4717-4bf9-9cf4-585b33e54629"),
+                column: "product_id",
+                value: new Guid("3af8f791-6c93-4afb-820d-3c8bccc83bc4"));
+
+            migrationBuilder.UpdateData(
+                table: "Repairs",
+                keyColumn: "id",
+                keyValue: new Guid("260e7b03-cf9e-4824-8e3d-174f6fb73e49"),
+                column: "product_id",
+                value: new Guid("3fc64654-f1e8-4563-ad8f-98c4c56a548a"));
+
+            migrationBuilder.UpdateData(
+                table: "Repairs",
+                keyColumn: "id",
+                keyValue: new Guid("29b9d37c-3f23-4507-96b5-8506b291218b"),
+                column: "product_id",
+                value: new Guid("338afc0c-7290-4c15-a5f5-cb091dea6ad3"));
+
+            migrationBuilder.UpdateData(
+                table: "Repairs",
+                keyColumn: "id",
+                keyValue: new Guid("530f0ff1-2d7e-4be0-80e3-77453658c9de"),
+                column: "product_id",
+                value: new Guid("9def4539-eb0b-4f28-ac0c-d7f4d7e18de0"));
+
+            migrationBuilder.UpdateData(
+                table: "Repairs",
+                keyColumn: "id",
+                keyValue: new Guid("633693ac-2891-4fa9-99e9-ffbb9d33aa4b"),
+                column: "product_id",
+                value: new Guid("d3bfdeae-ad9e-44e2-b666-52e758c19dda"));
+
+            migrationBuilder.UpdateData(
+                table: "Repairs",
+                keyColumn: "id",
+                keyValue: new Guid("7478668c-1c7a-47ad-82c8-48c3744a1fdb"),
+                column: "product_id",
+                value: new Guid("95bdd46a-f616-4188-9f1a-3dc2eb1a3a64"));
+
+            migrationBuilder.UpdateData(
+                table: "Repairs",
+                keyColumn: "id",
+                keyValue: new Guid("7d844508-2b92-4fe5-afc8-5e05382b9761"),
+                column: "product_id",
+                value: new Guid("23cbb20e-8174-4ab6-b32e-ee2010f19217"));
+
+            migrationBuilder.UpdateData(
+                table: "Repairs",
+                keyColumn: "id",
+                keyValue: new Guid("b70d7e3e-35c7-4c24-acda-1472ae0c1d55"),
+                column: "product_id",
+                value: new Guid("80abbca8-554d-4b16-b5de-024705497d4a"));
+
+            migrationBuilder.UpdateData(
+                table: "Repairs",
+                keyColumn: "id",
+                keyValue: new Guid("c016f3c3-5189-469a-a580-1798d88882ee"),
+                column: "product_id",
+                value: new Guid("cae134c8-1faf-4e65-b08d-864367afeb21"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repairs_product_id",
+                table: "Repairs",
+                column: "product_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Repairs_Products_product_id",
+                table: "Repairs",
+                column: "product_id",
+                principalTable: "Products",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
