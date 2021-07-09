4953eae5b633151f4f819b9046a15d3cb8a13eee
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-07-09 18:34:29 +0300

    Update ClientController.cs

--- Intership/Controllers/ClientController.cs
+++ Intership/Controllers/ClientController.cs
@@ -4,6 +4,10 @@ using System.Threading.Tasks;
 using Intership.Services.Abstracts;
 using Intership.Models.RequestModels.Client;
 using System.Linq;
+using Intership.Models.ResponseModels;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 
 namespace Intership.Controllers
 {
@@ -20,9 +24,12 @@ namespace Intership.Controllers
         }
 
         /// <summary>
-        /// Returns all clients or empty array if clients don`t exist in the database
+        /// Returns all clients
         /// </summary>
-        /// <returns></returns>
+        /// <response code="200">Success. Clients were received successfully</response>
+        /// <response code="500">Internal Server Error</response>
+        [ProducesResponseType(typeof(List<ClientResponseModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status500InternalServerError)]
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
@@ -32,11 +39,18 @@ namespace Intership.Controllers
         }
 
         /// <summary>
-        /// Returns a client or 404 status code if client doesn`t exist in the database
+        /// Returns a client
         /// </summary>
         /// <param name="id"></param>
-        /// <returns></returns>
-        [HttpGet("{id}", Name = "Get")]
+        /// <response code="200">Success. Client model was received successfully</response>
+        /// <response code="400">Bad request</response>
+        /// <response code="404">Client with provided id cannot be found</response>
+        /// <response code="500">Internal Server Error</response>
+        [ProducesResponseType(typeof(ClientResponseModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status500InternalServerError)]
+        [HttpGet("{id}")]
         public async Task<IActionResult> Get([FromRoute] Guid id)
         {
             if (!await _clientService.IsExist(id)) 
@@ -50,10 +64,15 @@ namespace Intership.Controllers
         }
 
         /// <summary>
-        /// Create a new client and returns added client id
+        /// Create a client
         /// </summary>
         /// <param name="model"></param>
-        /// <returns></returns>
+        /// <response code="201">Success. Status model was created successfully</response>
+        /// <response code="400">Bad Request</response>
+        /// <response code="500">Internal Server Error</response>
+        [ProducesResponseType(typeof(AddedResponseModel), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status500InternalServerError)]
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] AddClientModel model)
         {
@@ -64,14 +83,20 @@ namespace Intership.Controllers
 
             var addedClientId = await _clientService.CreateAsync(model);
 
-            return CreatedAtRoute("Get", new { id = addedClientId });
+            return CreatedAtAction("Get", new { id = addedClientId }, new AddedResponseModel(addedClientId, HttpStatusCode.Created));
         }
 
         /// <summary>
-        /// Delete a client and returns 200 status code or 404 status code if client doesn`t exist in the database
+        /// Delete a client
         /// </summary>
         /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id"></param>
+        /// <response code="204">Client was deleted successfully</response>
+        /// <response code="404">Client with provided id cannot be found</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status500InternalServerError)]
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete([FromRoute] Guid id)
         {
@@ -86,11 +111,18 @@ namespace Intership.Controllers
         }
 
         /// <summary>
-        /// Update a client and returns 200 status code or 404 status code if client doesn`t exist in the database
+        /// Update a client
         /// </summary>
         /// <param name="id"></param>
         /// <param name="model"></param>
-        /// <returns></returns>
+        /// <response code="200">Client was updated successfully</response>
+        /// <response code="400">Bad request</response>
+        /// <response code="404">Client with provided id cannot be found</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status500InternalServerError)]
         [HttpPut("{id}")]
         public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateClientModel model)
         {
@@ -106,14 +138,19 @@ namespace Intership.Controllers
 
             var updatedClientId = await _clientService.UpdateAsync(id, model);
 
-            return RedirectToAction("Get", "ClientController", new { id = updatedClientId });
+            return RedirectToAction("Get", new { id = updatedClientId });
         }
 
         /// <summary>
-        /// Returns a repairs for the client or 404 status code if client doesn`t exist in the database
+        /// Returns a repairs for the client
         /// </summary>
         /// <param name="id"></param>
-        /// <returns></returns>
+        /// <response code="200">Success. Repairs were received successfully</response>
+        /// <response code="404">Client with provided id cannot be found</response>
+        /// <response code="500">Internal Server Error</response>
+        [ProducesResponseType(typeof(List<RepairResponseModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(BaseResponseModel), StatusCodes.Status500InternalServerError)]
         [HttpGet("{id}/repairs")]
         public async Task<IActionResult> GetRepairs([FromRoute] Guid id)
         {
