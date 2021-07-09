4bb50ee508842a13eb93298a5fb6570d880865ea
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-07-09 17:40:38 +0300

    Added Tests

---
+++ Intership.Tests/Mocks/ClientFakeService.cs
@@ -0,0 +1,18 @@
+﻿using Intership.Services.Abstracts;
+using Moq;
+using System;
+
+namespace Intership.Tests.Mocks
+{
+    public class ClientFakeService
+    {
+        public Mock<IClientService> Mock;
+        public IClientService Service;
+
+        public ClientFakeService()
+        {
+            Mock = new Mock<IClientService>();
+            Service = Mock.Object;
+        }
+    }
+}
