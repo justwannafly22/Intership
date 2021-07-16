72322d835508a55aea5be6424438eaca5d842558
Egor <62179089+justwannafly22@users.noreply.github.com>
2021-07-16 14:08:58 +0300

    Update User.cs

--- Intership.Data/Entities/User.cs
+++ Intership.Data/Entities/User.cs
@@ -1,16 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Intership.Data.Entities
 {
     public class User : IdentityUser
     {
-        [Key]
-        [Column("id")]
-        public Guid Id { get; set; }
-
         [Required]
         public string Login { get; set; }
 
